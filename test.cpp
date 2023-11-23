d37aee8a64eec78aa7fbca9bbda3f8492756f851
Doxy <doxyai.yt@pm.me>
2023-11-22 20:06:04 -0800

    Test Driven Build Enviornment
    Added a build environment designed around test driven development with doctest.

---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest/doctest.h"
+
+#include "cpr/cpr.h"
+
+TEST_CASE("Hello World") {
+    std::cout << "Hello World" << std::endl;
+}
